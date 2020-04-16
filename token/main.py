1a11ffa438324f4a10a8ee4af3a9475e4b1d2847
Murph Murphy <murph@clurictec.com>
2020-04-16 11:23:32 -0600

    Add access logging

--- token/main.py
+++ token/main.py
@@ -1,8 +1,15 @@
 from aiohttp import web
 from settings import config
 from routes import setup_routes
+import logging
 
-app = web.Application()
+stdio_handler = logging.StreamHandler()
+stdio_handler.setLevel(logging.INFO)
+_logger = logging.getLogger('aiohttp.access')
+_logger.addHandler(stdio_handler)
+_logger.setLevel(logging.DEBUG)
+
+app = web.Application(logger=_logger)
 app['config'] = config
 setup_routes(app)
 web.run_app(app)
